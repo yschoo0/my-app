be423e8740e97d25cd48e94834c18b6b15ebdb13
yschoo0 <yyssms@daum.net>
2023-05-18 15:53:36 +0900

    best/new/header

--- src/App.js
+++ src/App.js
@@ -1,19 +1,30 @@
 import React from 'react';
 import './main.css';
 
+const Header = () => (
+  <header className="home-header">
+    <h2>한림대학교 일송기념 도서관</h2>
+    <h1>
+      <span></span> 신착/인기 도서 <span></span>
+    </h1>
+    <p>
+      awesome place to make oneself <br /> productive and entertained through
+      daily updates.
+    </p>
+  </header>
+);
+
 function Book(props) {
   return (
     <li className={`${props.className} book`}>
-      <a href={props.link}>
-        {props.children}
-      </a>
+      <a href={props.link}>{props.children}</a>
     </li>
   );
 }
 
 function Thin(props) {
   return (
-    <li className={`${props.className} thin`}> 
+    <li className={`${props.className} thin`}>
       <a href={props.link}>
         <p>{props.title}</p>
       </a>
@@ -30,32 +41,30 @@ function BookList(props) {
   );
 }
 
-
 function App() {
   return (
     <div>
+      <Header /> {}
       <BookList className="new" title="신착도서">
         <Book>책 제목 및 저자</Book>
-      <Book >책 제목 및 저자</Book>
-        <Book >책 제목 및 저자</Book>
-        <Thin  title="책 제목" />
-        <Book >책 제목 및 저자</Book>
-        <Book >책 제목 및 저자</Book>
-        <Book >책 제목 및 저자</Book>
+        <Book>책 제목 및 저자</Book>
+        <Book>책 제목 및 저자</Book>
+        <Thin title="책 제목" />
+        <Book>책 제목 및 저자</Book>
+        <Book>책 제목 및 저자</Book>
+        <Book>책 제목 및 저자</Book>
       </BookList>
       <BookList className="best" title="인기도서">
-        <Book >책 제목 및 저자</Book>
-        <Thin  title="책 제목" />
-        <Book >책 제목 및 저자</Book>
-        <Book >책 제목 및 저자</Book>
-        <Thin  title="책 제목" />
-        <Book >책 제목 및 저자</Book>
-        <Book >책 제목 및 저자</Book>
+        <Book>책 제목 및 저자</Book>
+        <Thin title="책 제목" />
+        <Book>책 제목 및 저자</Book>
+        <Book>책 제목 및 저자</Book>
+        <Thin title="책 제목" />
+        <Book>책 제목 및 저자</Book>
+        <Book>책 제목 및 저자</Book>
       </BookList>
     </div>
-    
-    );
+  );
 }
-
 
 export default App;
